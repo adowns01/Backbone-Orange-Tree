fd5971c01a4516be59c6ea71c4bab6fef7321784
adowns01 <downs.amelia@gmail.com>
2014-08-26 12:01:21 -0700

    added a constants object so the basic logic could be changed easily

--- treeView.js
+++ treeView.js
@@ -33,13 +33,13 @@ var TreeView = Backbone.View.extend({
 	getTreeImageURL: function() {
 		var treeAge = this.tree.get('age');
 		if (treeAge < this.tree.get('MIN_ORANGE_AGE')){
-			return "http://fe867b.medialib.glogster.com/media/dc/dc1a7adff862424cd98ee66089d7da17220379f5aa3af0d767382128be5a05ab/baby-tree.jpg"
+			return CONSTS.smallTreeSrc
 		} 
 		else if (treeAge > this.tree.get('MAX_ORANGE_AGE')){
-			return "http://fc02.deviantart.net/fs70/i/2011/152/9/3/dead_tree_png_by_gd08-d3hs9tf.png"
+			return CONSTS.largeTreeSrc
 		}
 		else {
-			return "http://www.plantsenhance.com/image/cache/data/artificial-orange-tree-150cm-500x700.jpg"
+			return CONSTS.mediumTreeSrc
 		}
 
 	}
